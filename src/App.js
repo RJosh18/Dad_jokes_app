d44125a796a83981e73157517233f7fd3cf139df
RJosh18 <riajoshi318@gmail.com>
2023-12-24 16:53:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import JokesList from "./JokesList";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <JokesList />
+    </div>
+  );
+}
+
+export default App;
